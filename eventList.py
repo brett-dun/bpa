e5dc2e38b56f7de3023773061c69ea8d772f9f63
Brett Duncan <brettd314@gmail.com>
2017-07-05 18:03:42 -0500

    new formatting, new pages

--- eventList.py
+++ eventList.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys
+import sys, random
 
 def main():
 
@@ -9,7 +9,8 @@ def main():
     for event in events:
         #print(event)
         data = event.split("\t")
-        print("<a href=" + str(data[1]).replace(' ', '').replace('/', '&') + ".html" + ">" + str(data[1]) + "</a><br>")
+        num = random.randint(1,3)
+        print("<button onclick="+'"'+"location.href = '" + str(data[1]).replace(' ', '').replace('/', '&') + ".html" + "';" + '" class="eventButton eventButton--'+ str(num) + '">' + str(data[1]) + "</button>")
 
 
 
---
+++ header.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    			var header = document.getElementById('headerBackground');
+    			if ( window.pageYOffset > 100 ) {
+    				header.style.background="#006fff"; }
+    			else {
+    				header.style.background="none";
+    			}
+			}
